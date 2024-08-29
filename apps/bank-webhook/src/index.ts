3de08cdf398fba79aca628453d1f2203e37d9103
Rahul Rawat <rahul24920@gmail.com>
2024-08-29 14:32:41 +0530

    transfer page and backend updates

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,38 +1,55 @@
 import express from "express";
 import db from "@repo/db/client";
-
 const app = express();
+
+app.use(express.json())
 
 app.post("/hdfcWebhook", async (req, res) => {
     //TODO: Add zod validation here?
-    // check if this request actually came from hdfc bank, use a webhook secret here
-    const paymentInformation = {
+    //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
+    const paymentInformation: {
+        token: string;
+        userId: string;
+        amount: string
+    } = {
         token: req.body.token,
         userId: req.body.user_identifier,
         amount: req.body.amount
     };
-    await db.balance.update({
-        where: {
-            userId: paymentInformation.userId
-        },
-        data: {
-            amount: {
-                increment: paymentInformation.amount
-            }
-        }
-    });
 
-    await db.onRampTransaction.update({
-        where: {
-            token: paymentInformation.token,
-        },
-        data: {
-            status: "Success"
-        }
-    });
+    try {
+        await db.$transaction([
+            db.balance.updateMany({
+                where: {
+                    userId: Number(paymentInformation.userId)
+                },
+                data: {
+                    amount: {
+                        // You can also get this from your DB
+                        increment: Number(paymentInformation.amount)
+                    }
+                }
+            }),
+            db.onRampTransaction.updateMany({
+                where: {
+                    token: paymentInformation.token
+                }, 
+                data: {
+                    status: "Success",
+                }
+            })
+        ]);
 
-    res.status(200).json({
-        message: "captured"
-    })
-    // Update balance in db, add txn
+        res.json({
+            message: "Captured"
+        })
+    } catch(e) {
+        console.error(e);
+        res.status(411).json({
+            message: "Error while processing webhook"
+        })
+    }
+
 })
+
+app.listen(3003);
